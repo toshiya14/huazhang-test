434857facb00b3ed595298a12c703e3fcd607897
Akaishi Toshiya <Toshiya14@live.jp>
2023-11-22 15:26:29 +0800

    init.

---
+++ TestConsole/Node.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    internal class Node
+    {
+        public Node? Left { get; set; }
+        public Node? Right { get; set; }
+        public int Value { get; set; }
+
+        public bool IsLeaf()
+        {
+            return this.Left is null && this.Right is null;
+        }
+
+        public override string ToString()
+        {
+            return $"{Value} | left:{Left?.Value} right:{Right?.Value}";
+        }
+    }
+
+    class NodeVisitor
+    {
+        public NodeVisitor(Node root)
+        {
+            this.Root = root;
+        }
+
+        public List<Node> GetLeaves()
+        {
+            var result = new List<Node>();
+            this.Visit(result, this.Root);
+            return result;
+        }
+
+        private void Visit(List<Node> history, Node node)
+        {
+            if (node.IsLeaf())
+            {
+                history.Add(node);
+                return;
+            }
+                 
+            if (node.Left is not null)
+            {
+                this.Visit(history, node.Left);
+            }
+
+            if (node.Right is not null)
+            {
+                this.Visit(history, node.Right);
+            }
+        }
+
+        public Node Root { get; }
+
+        public bool IsLeafAP() { 
+            var leaves = this.GetLeaves();
+            int? delta = null;
+            for (var i = 1; i < leaves.Count; i++)
+            {
+                if (delta is null)
+                {
+                    delta = leaves[i].Value - leaves[i - 1].Value;
+                }
+                if (leaves[i].Value - leaves[i - 1].Value != delta)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ TestConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+using TestConsole;
+
+var node3 = new Node { Value = 3 };
+var node5 = new Node { Value = 5 };
+var node9 = new Node { Value = 9, Left = node5, Right = node3 };
+var node1 = new Node { Value = 1 };
+var node6 = new Node { Value = 6 , Left = node1, Right = node9};
+var node7 = new Node { Value = 7 };
+var node4 = new Node { Value = 4 , Left = node6, Right = node7};
+
+var visitor = new NodeVisitor(node4);
+//var result = visitor.GetLeaves();
+var isap = visitor.IsLeafAP();
+Console.WriteLine(JsonSerializer.Serialize(isap));
+
+
